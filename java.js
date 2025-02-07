e6d24a36ce607ee6c0e24833f6133036c9e541cd
rr-tilly <88960346+rr-tilly@users.noreply.github.com>
2025-02-07 16:08:55 +0800

    Remove playGame() and added UI buttons for human player

--- java.js
+++ java.js
@@ -16,36 +16,55 @@ function getComputerChoice() {
 }
 
 //a function that takes the user choice and returns it
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper or scissors?", "").toLowerCase();
+// function getHumanChoice() {
 
-    switch (humanChoice) {
-        case 'rock' || 'rocks':
-            return 'Rock';
-            break;
-        case 'paper' || 'papers':
-            return 'Paper';
-            break;
-        case 'scissors' || 'scissors':
-            return 'Scissors';
-            break;
-        default:
-            console.log(`Sorry, not a valid choice`);
-    }
+//     const humanChoice = createButtons();
+
+//     switch (humanChoice) {
+//         case 'Rock':
+//             return 'Rock';
+//             break;
+//         case 'paper' || 'papers':
+//             return 'Paper';
+//             break;
+//         case 'scissors' || 'scissors':
+//             return 'Scissors';
+//             break;
+//     }
+
+// }
+
+//create button for player selection
+function createButtons() {
+    const buttonHolder = document.createElement("div")
+
+    const rockButton = document.createElement("button");
+    const paperButton = document.createElement("button");
+    const scissorsButton = document.createElement("button");
+
+    rockButton.textContent = "Rock";
+    paperButton.textContent = "Paper";
+    scissorsButton.textContent = "Scissors";
+
+    document.body.appendChild(buttonHolder);
+    buttonHolder.appendChild(rockButton);
+    buttonHolder.appendChild(paperButton);
+    buttonHolder.appendChild(scissorsButton);
+
+    rockButton.addEventListener("click", (e) => console.log(e.target.textContent));
+    paperButton.addEventListener("click", (e) => console.log(e.target.textContent));
+    scissorsButton.addEventListener("click", (e) => console.log(e.target.textContent));
 
 }
 
-//function to play a Single round
-function playRound(humanChoice, computerChoice) {
+function playRound() {
 
-
-    //get first letter of each choice
-    //add first letter to a a string called round; 
+    const humanChoice = createButtons();
+    const computerChoice = getComputerChoice();
 
     let round = humanChoice.charAt(0) + computerChoice.charAt(0);
     console.log(humanChoice, " vs ", computerChoice);
 
-    //compare round in switch statement where either player wins, loses, or ties.
     if (round === "PR" || round === "SP" || round === "RS") {
 
         console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
@@ -65,43 +84,33 @@ function playRound(humanChoice, computerChoice) {
     }
 };
 
-// play 5 rounds
+createButtons();
+// plays 5 rounds
+// function playGame() {
+//     // global variables to initialize player scores at 0;
+//     let humanScore = 0;
+//     let computerScore = 0;
 
+//     for (let i = 1; i <= 5; i++) {
 
-//write a function called playGame that calls playROUND 5 TIMES
-// keeps track of the scores and 
-// declares a winner at the end.
+//         console.log(`Round ${i}`);
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
 
-function playGame() {
-    // global variables to initialize player scores at 0;
-    let humanScore = 0;
-    let computerScore = 0;
+//         let winner = playRound(humanSelection, computerSelection);
+//         if (winner === 'human') {
+//             humanScore += 1;
+//             console.log(`[You: ${humanScore} | Computer: ${computerScore}]`)
+//         }
+//         else if (winner === 'pc') {
+//             computerScore += 1;
+//             console.log(`[You: ${humanScore} | Computer: ${computerScore}]`)
+//         } else { console.log(`[You: ${humanScore} | Computer: ${computerScore}]`) };
 
-    for (let i = 1; i <= 5; i++) {
-
-        console.log(`Round ${i}`);
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        let winner = playRound(humanSelection, computerSelection);
-        if (winner === 'human') {
-            humanScore += 1;
-            console.log(`[You: ${humanScore} | Computer: ${computerScore}]`)
-        }
-        else if (winner === 'pc') {
-            computerScore += 1;
-            console.log(`[You: ${humanScore} | Computer: ${computerScore}]`)
-        } else { console.log(`[You: ${humanScore} | Computer: ${computerScore}]`) };
-
-        if (i == 5) {
-            if (humanScore > computerScore) { console.log("You have won the game!") }
-            else if (humanScore < computerScore) { console.log("You have lost the game") }
-            else { (console.log("It's a tie!")) };
-        };
-    }
-
-
-
-}
-
-playGame();
+//         if (i == 5) {
+//             if (humanScore > computerScore) { console.log("You have won the game!") }
+//             else if (humanScore < computerScore) { console.log("You have lost the game") }
+//             else { (console.log("It's a tie!")) };
+//         };
+//     }
+// }
